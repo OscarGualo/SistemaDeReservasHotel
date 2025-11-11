59db2ddac97789895b5bec3ff8fb1797a8452f75
Tu Nombre <josd.1523@gmail.com>
2025-11-10 23:38:29 -0500

    Implementacionde la pantalla de login sea reesponsive

--- src/main/java/org/example/sistemareservas/HelloApplication.java
+++ src/main/java/org/example/sistemareservas/HelloApplication.java
@@ -1,19 +1,17 @@
 package org.example.sistemareservas;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.example.sistemareservas.Controlador.VentanaReserva;
-import org.example.sistemareservas.Modelo.Hotel;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Habitaciones.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 650);
 
         stage.setTitle("Hello!");
