4e5d7b2c085816f24201225528af118e2b864d0c
Oscar <oscar.gualotuna@epn.edu.ec>
2025-11-11 16:24:26 -0500

    Se arregla la clase servicios

--- src/main/java/org/example/sistemareservas/Modelo/Servicio.java
+++ src/main/java/org/example/sistemareservas/Modelo/Servicio.java
@@ -48,43 +48,5 @@ public class Servicio implements cobrable {
         return precio;
     }
 
-    public int getCodigo() {
-        return codigo;
-    }
 
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public boolean isEstado() {
-        return estado;
-    }
-
-    public void setEstado(boolean estado) {
-        this.estado = estado;
-    }
 }
